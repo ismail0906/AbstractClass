e0864ebf5acea557c32d0ef3e94d4a1be7fed1f9
ASUS <ASUS@ISMAIL>
2022-11-07 01:44:28 +0300

    Add project files.

---
+++ Kods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AbstractClass
+{
+    // Abstract Class ==> sınıfın sadece miras vermesi için işaretlenmesidir. Miras verebilir ancak yeni bir referans oluşturulmaz.
+    
+
+    public abstract class Personel
+    {
+        public string Adi { get; set; }
+
+        // Abstract üyelerin gövdesi olamaz.
+        public abstract string KimeBagli();
+    }
+
+    public class Yazilimci : Personel
+    {
+        public override string KimeBagli()
+        {
+            return "Bilişim Teknolojileri";
+        }
+
+    }
+    public class Muhasebeci : Personel 
+    {
+        public override string KimeBagli()
+        {
+            return "Finans";
+        }
+    }
+
+    public sealed class Departman // 'sealed' class ile işaretlenen sınıftan miras vermez
+    {
+
+    }
+
+
+    //public class IK : Departman  // Departman  sınıfı sealed olduğu için miras veremez
+    //{
+
+    //}
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _6_AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Yazilimci yz = new Yazilimci();
+            yz.Adi = "Ekrem";
+
+            Yazilimci yz1 = new Yazilimci();
+            yz.Adi = "İsmail";
+
+            Muhasebeci mh = new Muhasebeci();
+            mh.Adi = "Büşra";
+
+            Personel p1 = mh; // p1 mh'nin referansını tutuyor..
+
+            // Personel p = new Personel(); // Personel sınıfımız Abstract olduğu için bu sınıftan instance alınamaz.
+
+            Console.WriteLine(yz.KimeBagli());
+            Console.WriteLine(yz1.KimeBagli());
+            Console.WriteLine(mh.KimeBagli());
+
+            Departman dep =new Departman();
+
+            OdemeYap(mh);
+            OdemeYap(yz1);
+            OdemeYap(yz);
+
+
+            Console.ReadKey();
+        }
+        static void OdemeYap(Personel p) 
+        {
+        }
+    }
+}
